a81874248a8a890fd0150b7dfb86e40e78c89079
RakaDoank <rakaaudira@gmail.com>
2026-06-27 20:58:05 +0700

    release uws-react-native to GitHub packages

---
+++ scripts/release-github.mjs
@@ -0,0 +1,52 @@
+import node_childProcess from "node:child_process"
+import node_fs from "node:fs"
+import node_path from "node:path"
+
+const
+	rootDir =
+		node_path.join(import.meta.dirname, ".."),
+
+	packageJsonFilePath =
+		node_path.join(
+			rootDir,
+			"package",
+			"package.json",
+		)
+
+let
+	packageJson =
+		JSON.parse(
+			node_fs.readFileSync(
+				packageJsonFilePath,
+				"utf8",
+			),
+		)
+
+// We have to use organization name or scope name for the library's name
+// Change the `uws-react-native` to `@rakadoank/uws-react-native`
+packageJson.name = "@rakadoank/uws-react-native"
+
+// https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-npm-registry#publishing-a-package-using-publishconfig-in-the-packagejson-file
+packageJson.publishConfig = {
+	registry: "https://npm.pkg.github.com",
+}
+
+node_fs.writeFileSync(
+	packageJsonFilePath,
+	JSON.stringify(
+		packageJson,
+		null,
+		2,
+	),
+	{
+		encoding: "utf8",
+	},
+)
+
+node_childProcess.execSync(
+	"pnpm publish --filter uws-react-native --access public --no-git-checks",
+	{
+		cwd: rootDir,
+		stdio: "inherit",
+	},
+)
