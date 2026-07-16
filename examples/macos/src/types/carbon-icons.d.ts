bce5a8c3630c16b1e725df722bfcdc7f411bfaa5
RakaDoank <rakaaudira@gmail.com>
2026-07-16 15:36:11 +0700

    fix type check

---
+++ examples/macos/src/types/carbon-icons.d.ts
@@ -0,0 +1,8 @@
+declare module "@carbon/icons/svg/*.svg" {
+	import type {
+		SvgProps,
+	} from "react-native-svg"
+
+	const Component: React.FunctionComponent<SvgProps>
+	export default Component
+}
